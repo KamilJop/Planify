3ceff992ad99a40c6d6e75e2f39c82171ccd32c5
KamilJop <kamiljop434@gmail.com>
2025-03-05 23:51:40 +0100

    Update _layout.tsx

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,13 +1,16 @@
-import { View, Text } from 'react-native'
+import { View, Text, StatusBar } from 'react-native'
 import React from 'react'
 import { Stack } from 'expo-router'
 
 const AuthLayout = () => {
   return (
-    <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="index"  />
-      <Stack.Screen name="register"  />
-    </Stack>
+    <>
+      <StatusBar translucent backgroundColor="transparent" barStyle="light-content" />
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="index" />
+        <Stack.Screen name="register" />
+      </Stack>
+    </>
   )
 }
 
