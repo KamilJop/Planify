611c3050083aa24832e6c5ad6f141acf117d38c6
KamilJop <kamiljop434@gmail.com>
2025-04-28 23:46:24 +0200

    design update

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -6,7 +6,9 @@ import { BellIcon, CloseCircleIcon, Icon } from '@/components/ui/icon'
 import { ChevronDownIcon } from "@/components/ui/icon"
 import { Picker } from '@react-native-picker/picker'
 import { useTheme } from '@/components/ThemeContext'
-
+import { Divider } from "@/components/ui/divider"
+import { VStack } from '@/components/ui/vstack'
+import { HStack } from '@/components/ui/hstack'
 const Settings = () => {
   const { theme, toggleTheme, colors } = useTheme()
   const [notifications, setNotifications] = React.useState(true)
@@ -20,6 +22,50 @@ const Settings = () => {
       justifyContent: 'center', 
       alignItems: 'center'
     }}>
+
+      <VStack style={{ width: width, height: 80, alignItems: 'center', backgroundColor: colors.surface, borderRadius: 10, margin: 10 }}>
+        <HStack style={{ width: width, justifyContent: 'space-between', alignItems: 'center', padding: 10 }}>
+
+      <Text style={{ color: theme === 'dark' ? 'white' : 'black', marginRight: 10, marginLeft: 10, fontSize: 15 }}>
+          Theme
+      </Text>
+      <Text style={{ color: theme === 'dark' ? 'white' : 'black', marginRight: 10, marginLeft: 10, fontSize: 15 }}>
+          {theme === 'dark' ? 'Dark Mode' : 'Light Mode'}
+      </Text>
+         </HStack>
+      <Divider style={{ backgroundColor: 'gray', height: 1, width: '90%' }} />
+
+      <HStack style={{ width: width, justifyContent: 'space-between', alignItems: 'center', padding: 10 }}>
+        <Text style={{ color: theme === 'dark' ? 'white' : 'black', marginRight: 10, marginLeft: 10, fontSize: 15 }}>
+          Accent Color
+        </Text>
+        <HStack style={{ flexDirection: 'row', alignItems: 'center', gap : 10, marginRight: 10 }}>
+          <View style={{backgroundColor:'#a258d6', width:20, height:20 , borderColor:'#a258d6', borderRadius:100, borderWidth:1 }}> </View>
+          <View style={{backgroundColor:'#7c4cd1', width:20, height:20 , borderColor:'#7c4cd1', borderRadius:100, borderWidth:1 }}> </View>
+          <View style={{backgroundColor:'#3497b1', width:20, height:20 , borderColor:'#3497b1', borderRadius:100, borderWidth:1 }}> </View>
+          <View style={{backgroundColor:'#299876', width:20, height:20 , borderColor:'#299876', borderRadius:100, borderWidth:1 }}> </View>
+          <View style={{backgroundColor:'red', width:20, height:20 , borderColor:'#c43540', borderRadius:100, borderWidth:1 }}> </View>
+        </HStack>
+        
+      </HStack>
+      </VStack>
+      <VStack style={{ width: width, height: 120, alignItems: 'center', backgroundColor: colors.surface, borderRadius: 10, padding: 5, margin: 10 }}>
+        <HStack style={{ width: width, alignItems: 'center', padding: 10 }}>
+          <View style={{ backgroundColor: 'gray', borderColor: 'gray', borderRadius: 100, borderWidth: 1, height: 50, width: 50, marginLeft: 5, marginTop: 5 }}> </View>
+          <Text style={{ color: theme === 'dark' ? 'white' : 'black', marginRight: 10, marginLeft: 10, fontSize: 15 }}>
+        Change Username
+          </Text>
+        </HStack>
+        <Divider style={{ backgroundColor: 'gray', height: 1, width: '90%' , marginBottom:10}} />
+        <HStack style={{ width: width, alignItems: 'center', paddingLeft: 15 }}>
+          <Text style={{ color: theme === 'dark' ? 'white' : 'black', fontSize: 15, marginLeft: 5 }}>
+        Change Profile Picture
+          </Text>
+        </HStack>
+      </VStack>
+
+  
+
       <View style={{
         flexDirection: 'row', 
         alignItems: 'center', 
@@ -56,7 +102,7 @@ const Settings = () => {
         </Text>
       </View>
 
-      {/* Notifications Toggle - Preserving your original style */}
+
       <View style={{
         flexDirection: 'row', 
         alignItems: 'center', 
